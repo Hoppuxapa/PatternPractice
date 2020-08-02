f271e44ff4eb8c1d3075d409045f24d2bff0a2db
Ruslan <hoppuxapa@gmail.com>
2020-08-03 01:20:23 +0300

    Add FactoryMethod

---
+++ src/FactoryMethod.java
@@ -0,0 +1,17 @@
+public class FactoryMethod {
+
+    public static void main(String[] args) {
+        Car car = Car.create();
+    }
+
+
+    static class Car {
+        public Car() {
+        }
+
+        public static Car create() {
+            System.out.println("You have created new Car");
+            return new Car();
+        }
+    }
+}
