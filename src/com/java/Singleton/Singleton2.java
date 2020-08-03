46db5cde5a2fb87dd849494e4e11afeefc88eddd
Ruslan <hoppuxapa@gmail.com>
2020-08-03 11:24:55 +0300

    Add Singleton2 with Double Checked Locking and volatile

---
+++ src/com/java/Singleton/Singleton2.java
@@ -0,0 +1,19 @@
+package com.java.Singleton;
+
+public class Singleton2 {
+    private static volatile Singleton2 instance;
+
+    //Double Checked Locking and volatile
+    public static Singleton2 getInstance() {
+        Singleton2 localInstance = instance;
+        if (localInstance == null) {
+            synchronized (Singleton.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new Singleton2();
+                }
+            }
+        }
+        return localInstance;
+    }
+}
